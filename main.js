1a3fcc4ab27764c76d76654fe648944cc2bf6174
vandana-eva <92171611+vandana-eva@users.noreply.github.com>
2022-03-29 15:53:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,3 +31,94 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance (speak_data_1+speak_data_2);
     synth.speak(utterThis);
 }  
+function check()
+{
+img=document.getElementById('captured_image');
+classifier.classify(img,gotResult);
+}  
+
+function gotResult(error,result)
+{
+if (error)
+{
+    console.error(error);
+}
+else 
+{
+    console.log(result);
+    document.getElementById("result_emotion_name").innerHTML=result[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=result[1].label;
+    prediction_1 = result[0].label;
+    prediction_2 = result[1].label;
+    speak();
+    if (result[0].label=="one")
+    {
+        document.getElementById("update_emoji").innerHTML="&#9757;";
+    }
+
+    if (result[0].label=="fist")
+    {
+        document.getElementById("update_emoji").innerHTML="&#9994;";
+    }
+    
+    if (result[0].label=="hand")
+    {
+        document.getElementById("update_emoji").innerHTML="&#9995;";
+    }
+    
+    if (result[0].label=="two")
+    {
+        document.getElementById("update_emoji").innerHTML="&#9996;";
+    }
+    
+    if (result[0].label=="thumps up")
+    {
+        document.getElementById("update_emoji").innerHTML="&#128077;";
+    }
+    
+    if (result[0].label=="thumps down")
+    {
+        document.getElementById("update_emoji").innerHTML="&#128078;";
+    }
+    
+    if (result[0].label=="horns")
+    {
+        document.getElementById("update_emoji").innerHTML="&#129304;";
+    }
+
+    if (result[1].label=="one")
+    {
+        document.getElementById("update_emoji").innerHTML="&#9757;";
+    }
+
+    if (result[1].label=="fist")
+    {
+        document.getElementById("update_emoji").innerHTML="&#9994;";
+    }
+    
+    if (result[1].label=="hand")
+    {
+        document.getElementById("update_emoji").innerHTML="&#9995;";
+    }
+    
+    if (result[1].label=="two")
+    {
+        document.getElementById("update_emoji").innerHTML="&#9996;";
+    }
+    
+    if (result[1].label=="thumps up")
+    {
+        document.getElementById("update_emoji").innerHTML="&#128077;";
+    }
+    
+    if (result[1].label=="thumps down")
+    {
+        document.getElementById("update_emoji").innerHTML="&#128078;";
+    }
+    
+    if (result[1].label=="horns")
+    {
+        document.getElementById("update_emoji").innerHTML="&#129304;";
+    }
+}
+}
